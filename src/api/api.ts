f7fd7ba02a19235b3f3923e8aec8e2eb61cad241
SnezhkoAndrey <92161597+SnezhkoAndrey@users.noreply.github.com>
2023-02-03 12:55:46 +0200

    refactoring api

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,47 +1,36 @@
-import axios from 'axios'
 import { DetectedText, HTTPClientType, SupportedLanguage, TranslateText } from '../types/types'
 
 const HTTPClient = (): HTTPClientType => {
-  const baseURL = 'https://translate-language.p.rapidapi.com/'
+  const baseURL = process.env.REACT_APP_URL as string
   const headers = {
     'content-type': 'application/json',
     'X-RapidAPI-Key': process.env.REACT_APP_API as string,
     'X-RapidAPI-Host': 'translate-language.p.rapidapi.com',
   }
 
-  async function axiosRequest(endpoint: string, option = {}): Promise<SupportedLanguage> {
-    const options = {
-      ...option,
-      url: baseURL + endpoint,
-      headers: {
-        'X-RapidAPI-Key': process.env.REACT_APP_API,
-        'X-RapidAPI-Host': 'translate-language.p.rapidapi.com',
-      },
-    }
-    const response = await axios.request(options)
-    return response.data as SupportedLanguage
-  }
-
-  async function fetchJSON(endpoint: string, options = {}): Promise<TranslateText | DetectedText> {
+  async function fetchJSON(
+    endpoint: string,
+    options = {},
+  ): Promise<TranslateText | DetectedText | SupportedLanguage> {
     const response = await fetch(baseURL + endpoint, {
       ...options,
-      headers: headers,
+      headers,
     })
 
-    const data = (await response.json()) as TranslateText | DetectedText
+    const data = (await response.json()) as TranslateText | DetectedText | SupportedLanguage
 
     return data
   }
 
-  const POST = async (endpoint: string, value: string): Promise<TranslateText | DetectedText> => {
+  const POST = async (endpoint: string, value: string): Promise<any> => {
     return await fetchJSON(endpoint, {
       method: 'POST',
       body: `{"text":"${value}"}`,
     })
   }
 
-  const GET = async (endpoint: string): Promise<SupportedLanguage> => {
-    return await axiosRequest(endpoint, {
+  const GET = async (endpoint: string): Promise<any> => {
+    return await fetchJSON(endpoint, {
       method: 'GET',
     })
   }
